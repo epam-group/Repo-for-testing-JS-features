91bd51d0c21ab3cbb5e4fbe819a7bcbe0d304719
Maxim <maxnuzhdin@icloud.com>
2019-10-19 23:57:43 +0300

    1.1
    added bug report

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JSLearn
- for practice
+ for practice JS on creating
+ bot`s for discord/telegram/VK 
+ and other services
--- botbody.js
+++ botbody.js
@@ -4,6 +4,7 @@ let config = require(`./botconfig.json`);
 let token = config.token;
 let prefix = config.prefix;
 
+bot.login(token);
 bot.on(`ready`,() => {
     console.log(`Logged here as ${bot.user.username}!`);
     });
@@ -13,4 +14,23 @@ bot.on(`message`, msg => {
             msg.reply(`pong!`);
         }
     });
-bot.login(token);
+bot.on(`message`, msg => {
+    if (msg.content === `anime`) {
+        msg.reply(`https://www.yummyclub.com/milosjke`);
+        }
+    });
+bot.on(`message`, msg => {
+        if (msg.content === `vk`) {
+            msg.reply(`https://vk.com/milosj`);
+         }
+  });
+bot.on(`message`, msg => {
+    if (msg.content === `anime`) {
+        msg.reply(`https://www.yummyclub.com/milosjke`);
+        }
+    });
+bot.on(`message`, msg => {
+        if (msg.content === `bets`) {
+            msg.reply(`https://www.boosty.to/milosj`);
+        }
+     });
